5a92248f50719df5d23108120c2d38ce01bef019
Terrence Dominguez-Smith <domingte@oregonstate.edu>
2020-04-01 22:44:26 -0700

    .

---
+++ basic_stats.py
@@ -0,0 +1,5 @@
+# Author: Terrence Dominguez-Smith
+# Date: 4/1/2020
+# Description: This program imports the statistics module and collects peoples name and ages which will create tuples
+# of the mean, median, and mode of their ages.
+
